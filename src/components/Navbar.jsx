5bcd213ea2b2d54ea3fc5cfe920d5a461b3db040
dadler83 <113302798+dadler83@users.noreply.github.com>
2026-03-04 21:56:20 -0500

    added white svgs for scrolled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { Link, NavLink } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import githubIcon from '../assets/GitHub_Invertocat_Black.svg'
-import linkedInIcon from '../assets/linkedin-svgrepo-com.svg'
+import githubIconBlack from '../assets/GitHub_Invertocat_Black.svg'
+import githubIconWhite from '../assets/GitHub_Invertocat_White.svg'
+import linkedInIconBlack from '../assets/linkedin-svgrepo-com.svg'
+import linkedInIconWhite from '../assets/linkedin-svgrepo-com-white.svg'
 import './Navbar.css'
 
 export default function Navbar() {
@@ -84,12 +86,12 @@ export default function Navbar() {
                 <ul className="navbar-menu">
                     <li className="" style={{paddingTop: '5px'}}>
                         <a href="https://www.linkedin.com/in/david-adler-a30b3424a/" target="_blank" rel="noopener noreferrer">
-                            <img height={30} src={linkedInIcon} alt="LinkedIn" className="navbar-icon" />
+                            <img height={30} src={isScrolled ? linkedInIconWhite : linkedInIconBlack} alt="LinkedIn" className="navbar-icon" />
                         </a>
                     </li>
                     <li className="" style={{paddingTop: '10px'}}>
                         <a href="https://github.com/dadler83" target="_blank" rel="noopener noreferrer">
-                            <img height={20} src={githubIcon} alt="Github" className="navbar-icon" />
+                            <img height={20} src={isScrolled ? githubIconWhite : githubIconBlack} alt="Github" className="navbar-icon" />
                         </a>
                     </li>
                 </ul>
