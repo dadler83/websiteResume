5b3f752ef27489325c9523649b307945d8661352
dadler83 <113302798+dadler83@users.noreply.github.com>
2026-03-22 20:09:28 -0400

    moved files

--- src/components/TetrisSketch.jsx
+++ src/components/TetrisSketch.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useRef, useState} from 'react'
 import p5 from 'p5'
-import { tetrisSketch } from '../tetris.js'
+import { tetrisSketch } from './tetris.js'
 
 export default function TetrisSketch() {
     const containerRef = useRef(null)
